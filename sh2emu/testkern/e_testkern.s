46413dfbdd3a3a5c91164cfadfb599c8d8520bd2
cr88192 <cr88192@gmail.com>
2016-09-15 01:24:00 -0500

    tweaks

--- sh2emu/testkern/e_testkern.s
+++ sh2emu/testkern/e_testkern.s
@@ -1,10 +1,25 @@
+#ifdef ARCH_SH4
+#define X(n)	n
+#define STACK_BASE	0x8FFFFFFC
+#else
+#define X(n)	_##n
+#define STACK_BASE	0x00003FFC
+#endif
+
+.section .bss
+.global X(timer_ticks)
+X(timer_ticks): .long 0
+
 .section .text
 .align 2
 .global _start
 .global start
 .global _vector_table
-.extern _tk_main
-.extern _timer_ticks
+.extern X(tk_main)
+.extern X(timer_ticks)
+
+.extern X(__bss_start)
+.extern X(_end)
 
 start:
 _start:
@@ -13,18 +28,44 @@ _start:
 	mov.l pio_addr, r1
 	mov.l r0, @r1
 
+	mov.l stack_base, sp
+
+	bsr zero_bss
+	nop
+
 	mov.l isr_table, r0
 	ldc r0, vbr
 	
 	mov #0, r0
 	ldc r0, sr
 
-	mov.l tk_main, r0
+	mov.l a_tk_main, r0
 	jsr @r0
 	nop
 
-	bra _loop
+//	bra _loop
+	bra _exit
 	nop
+
+zero_bss:
+	mov.l bss_strt, r2
+	mov.l bss_end, r3
+	mov #0, r4
+.L0:
+	mov.l r4, @-r3
+//	cmp/hs r2, r3
+	cmp/hi r2, r3
+	bt .L0
+	nop
+	rts
+	nop
+	bra _exit
+
+.align 4
+bss_strt: .long __bss_start
+bss_end: .long _end
+stack_base: .long STACK_BASE
+
 
 _loop:
 	bra _loop
@@ -95,7 +136,7 @@ _gdb_pit_isr:
 	nop
 
 .align 4
-ticks: .long _timer_ticks
+ticks: .long X(timer_ticks)
 
 _gdb_ihandler_uart1:
 	rte
@@ -107,10 +148,142 @@ start_leds:   .long 0x000000ff
 start1_leds:   .long 0x0000004f
 start2_leds:   .long 0x0000004e
 
-tk_main: .long _tk_main
+.long 0x5A5A5A5A
+a_tk_main: .long X(tk_main)
 .long 0x5B5B5B5B
 isr_table: .long _vector_table
 .long 0x5C5C5C5C
+
+
+#ifdef ARCH_SH2
+.global X(__ashrsi3)
+X(__ashrsi3):
+	not r5, r5
+	mov	#31,r0
+	and	r0,r5
+	shal r5
+	mova shar_list,r0
+	add r0, r5
+	mov r4, r0
+	jmp @r5
+	nop
+.align 2
+shar_list:
+	shar	r0	/* 31 */
+	shar	r0	/* 30 */
+	shar	r0	/* 29 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 25 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 21 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 17 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 13 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 9 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 5 */
+	shar	r0
+	shar	r0
+	shar	r0
+	shar	r0	/* 1 */
+	rts
+	nop
+
+.global X(__ashlsi3)
+X(__ashlsi3):
+	mov r5, r0
+.global X(__ashlsi3_r0)
+X(__ashlsi3_r0):
+	mov.l r4, @-sp
+	not r0, r0
+	and	#31,r0
+	shal r0
+	mov r0, r4
+	mova shll_list,r0
+	add r0, r4
+//	mov r4, r0
+	mov.l @sp+, r0
+	jmp @r4
+	nop
+.align 2
+shll_list:
+	shll	r0	/* 31 */
+	shll	r0	/* 30 */
+	shll	r0	/* 29 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 25 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 21 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 17 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 13 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 9 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 5 */
+	shll	r0
+	shll	r0
+	shll	r0
+	shll	r0	/* 1 */
+	rts
+	nop
+
+.global X(__ashiftrt_r4_12)
+.global X(__ashiftrt_r4_11)
+.global X(__ashiftrt_r4_10)
+.global X(__ashiftrt_r4_9)
+.global X(__ashiftrt_r4_8)
+.global X(__ashiftrt_r4_7)
+.global X(__ashiftrt_r4_6)
+.global X(__ashiftrt_r4_5)
+.global X(__ashiftrt_r4_4)
+.global X(__ashiftrt_r4_3)
+.global X(__ashiftrt_r4_2)
+.global X(__ashiftrt_r4_1)
+.global X(__ashiftrt_r4_0)
+X(__ashiftrt_r4_12):	shar r4
+X(__ashiftrt_r4_11):	shar r4
+X(__ashiftrt_r4_10):	shar r4
+X(__ashiftrt_r4_9):		shar r4
+X(__ashiftrt_r4_8):		shar r4
+X(__ashiftrt_r4_7):		shar r4
+X(__ashiftrt_r4_6):		shar r4
+X(__ashiftrt_r4_5):		shar r4
+X(__ashiftrt_r4_4):		shar r4
+X(__ashiftrt_r4_3):		shar r4
+X(__ashiftrt_r4_2):		shar r4
+X(__ashiftrt_r4_1):		shar r4
+X(__ashiftrt_r4_0):		rts
+						nop
+
+#endif
 
 /* .section .vect */
 .align 4
@@ -186,5 +359,12 @@ _vector_table:
 .long _gdb_unhandled_isr
 .long _gdb_unhandled_isr
 
+
+.section .data
+.weak X(__fpscr_values)
+.global X(__fpscr_values)
+X(__fpscr_values):
+	.long   0
+	.long   0x80000
 
 .end
--- sh2emu/testkern/tk_build.sh
+++ sh2emu/testkern/tk_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 export PATH=/opt/cross/bin:$PATH
-export "TK_LDFL=-nostdinc -nostdlib -nostartfiles -fno-builtin \
+export "TK_CFL=-nostdinc -nostdlib -nostartfiles -fno-builtin -DARCH_SH2"
+export "TK_LDFL=$TK_CFL \
 	-Wl,-nostdlib -Wl,-e,_start -Wl,-T,shelf.x"
 export "TK_CSRC=tkmain.c"
 
-sh2-elf-as e_testkern.s -o e_testkern.o
+# sh2-elf-as e_testkern.S -o e_testkern.o
+sh2-elf-gcc -c e_testkern.S -o e_testkern.o $TK_CFL
 
 sh2-elf-gcc -o ../testkern.elf e_testkern.o $TK_CSRC $TK_LDFL
